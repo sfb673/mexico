4c527ab71b12013b85784134dd4275d1419123bb
Peter Menke <pmenke@googlemail.com>
2012-12-07 15:04:16 +0100

    removed deprecated file, added media type support

---
+++ lib/mexico/core/media_type.rb
@@ -0,0 +1,29 @@
+# This file is part of the MExiCo gem.
+# Copyright (c) 2012 Peter Menke, SFB 673, Universität Bielefeld
+# http://www.sfb673.org
+#
+# MExiCo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# MExiCo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MExiCo. If not, see
+# <http://www.gnu.org/licenses/>.
+
+class Mexico::Core::MediaType
+  
+  attr_accessor :identifier, :name, :extensions
+  
+  def initialize(opts={})
+    [:identifier,:name,:extensions].each do |att|
+      send("#{att}=", opts[att]) if opts.has_key?(att)
+    end
+  end
+  
+end
