ace7f954d7797c1c8deb509db654acc1fc2906df
Peter Menke <pmenke@googlemail.com>
2013-02-15 14:20:22 +0100

    added first method, first spec
    Signed-off-by: Peter Menke <pmenke@googlemail.com>

---
+++ spec/file_system_based/inter_links_spec.rb
@@ -0,0 +1,40 @@
+# This file is part of the MExiCo gem.
+# Copyright (c) 2012 Peter Menke, SFB 673, Universität Bielefeld
+# http://www.sfb673.org
+#
+# MExiCo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# MExiCo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MExiCo. If not, see
+# <http://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+
+describe Mexico::FileSystem::Resource do
+  
+  # set up an initial corpus representation from the example file
+  before(:each) do
+    @basepath = File.join(File.dirname(__FILE__), '..','..','assets','mexico-testcorpus','utterance')
+    @xml = File.open(File.join(@basepath,'Corpus.xml'),'rb') { |f| f.read }
+    @corpus = Mexico::FileSystem::Corpus.from_xml(@xml, {:path => @basepath})
+    @resource = @corpus.resources.first
+    @local_file = @resource.local_files[0]
+  end
+  
+  context 'our little corpus on the prairie' do
+  
+    it 'should link from design to all trials' do
+      1.should eq 1  
+    end
+    
+  end
+
+end
