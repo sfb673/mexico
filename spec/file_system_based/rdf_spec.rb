3d157c09206ec37bdd6faa80925cdb42e758c460
Peter Menke <pmenke@googlemail.com>
2013-06-23 17:19:47 +0200

    spec file for rdf stuff
    Signed-off-by: Peter Menke <pmenke@googlemail.com>

--- spec/file_system_based/rdf_spec.rb
+++ spec/file_system_based/rdf_spec.rb
@@ -30,29 +30,77 @@ describe Mexico::FileSystem::Corpus do
 
   end
 
-  # set up an initial corpus representation from the example file
-  before(:each) do
-    @basepath = File.join(File.dirname(__FILE__), '..','..','assets','mexico-testcorpus', 'mexico')
-    @corpus = Mexico::FileSystem::Corpus.open(@basepath)
-    @corpus.resources[1].load
-  end
+
 
   context 'Corpus' do
 
+    # set up an initial corpus representation from the example file
+    before(:each) do
+      @basepath = File.join(File.dirname(__FILE__), '..','..','assets','mexico-testcorpus', 'mexico')
+      @corpus = Mexico::FileSystem::Corpus.open(@basepath)
+      @corpus.resources[1].load
+    end
+
     it 'should output RDF ntriples' do
       rdf = @corpus.to_rdf
-      puts rdf
+      File.open(File.join(File.dirname(__FILE__), '..','..','assets','rdf-output','corpus.nt'), 'w:utf-8') do |f|
+        f << rdf
+      end
       rdf.should_not be nil
     end
 
     it 'should output RDF RDFXML' do
       rdf = @corpus.to_rdf(:format => :rdfxml, :base_uri => 'http://phoibos.sfb673.org/corpora/',
                            :prefixes => {:cats=>'http://cats.sfb673.org/',:phoibos=>'http://phoibos.sfb673.org/corpora/'})
-      puts rdf
+      File.open(File.join(File.dirname(__FILE__), '..','..','assets','rdf-output','corpus.rdf'), 'w:utf-8') do |f|
+        f << rdf
+      end
       rdf.should_not be nil
     end
 
+  end
+
+  context 'Fiesta Document' do
+
+    before :each do
+      @basepath = File.join(File.dirname(__FILE__), '..','..','assets','mexico-testcorpus', 'mexico')
+      @corpus = Mexico::FileSystem::Corpus.open(@basepath)
+      @corpus.resources[1].load
+      @document = @corpus.resources[1].document
+    end
+
+    it 'should output RDF XML' do
+      rdf = @document.to_rdf(:format => :rdfxml, :base_uri => 'http://phoibos.sfb673.org/corpora/',
+                             :prefixes => {:cats=>'http://cats.sfb673.org/',:phoibos=>'http://phoibos.sfb673.org/corpora/'})
+      File.open(File.join(File.dirname(__FILE__), '..','..','assets','rdf-output','document.rdf'), 'w:utf-8') do |f|
+        f << rdf
+      end
+      rdf.should_not be nil
+    end
 
   end
 
+  context 'B6 Document' do
+
+    before :each do
+      @basepath = File.join(File.dirname(__FILE__),'..','..','assets','fiesta', 'b6')
+      @filename = File.join @basepath, 'match_jones_161_CM_neu_checked.parsed.xml'
+      @document = ::Mexico::Fiesta::Interfaces::B6ChatGameInterface.instance.import(File.read(@filename))
+      puts "DCN: %s " % @document.class.name
+      @document.self_uri('http://phoibos.sfb673.org/corpora/example/b6.fst')
+    end
+
+    it 'should output RDF XML' do
+      rdf = @document.to_rdf(:format => :rdfxml, :base_uri => 'http://phoibos.sfb673.org/corpora/',
+                             :prefixes => {:cats=>'http://cats.sfb673.org/',:phoibos=>'http://phoibos.sfb673.org/corpora/'})
+      File.open(File.join(File.dirname(__FILE__), '..','..','assets','rdf-output','b6.rdf'), 'w:utf-8') do |f|
+        f << rdf
+      end
+      File.open(File.join(File.dirname(__FILE__), '..','..','assets','rdf-output','b6.fst'),'w') do |f|
+        f << @document.to_xml
+      end
+      rdf.should_not be nil
+    end
+
+  end
 end
